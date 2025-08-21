2903ae2594434e6239344da05494074590a9451a
Marcantonio <marcantonio.lupercino@gmail.com>
2025-08-21 19:20:39 -0300

    for idade

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log(`Hello world`)
+const readline = require (`readline`);
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+function nota(pergunta) {
+  return new Promise((resolve) => {
+    rl.question(pergunta, (resposta) => {
+      resolve(Number(resposta));
+    });
+  });
+}
+
+async function perguntaIdade(){
+    const idade = await nota (`Qual sua idade? `);
+
+    for (let i =1; i <= idade; i++){
+        console.log(`sua idade é: ${i}`);
+    }
+    rl.close();
+}
+perguntaIdade();
