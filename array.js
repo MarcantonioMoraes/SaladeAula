3af76e1850bc4272ba69fc90c5a2b968e8c0c8ec
Marcantonio <marcantonio.lupercino@gmail.com>
2025-08-26 20:41:12 -0300

    Update array.js

--- array.js
+++ array.js
@@ -8,36 +8,50 @@
 // }
 // console.log(inteiros);
 // console.log(inversa.reverse());
-const readline = require('readline');
-const prompt = readline.createInterface({
-    input: process.stdin, output: process.stdout
-});
 
-function perguntar(pergunta) {
-    return new Promise((resolve) => {
-        prompt.question(pergunta, (resposta) => {
-            const valor = Number(resposta.replace(',', '.'));
-            resolve(valor);
-        });
-    });
-}
+// const readline = require('readline');
+// const prompt = readline.createInterface({
+//     input: process.stdin, output: process.stdout
+// });
 
-async function medalhaDeAcordoComPosicao () {
-    let numero = await perguntar('Digite sua posição na corrida (1, 2 ou 3): ');
-    const posicoes = ["ouro", "prata", "bronze", "Nada"];
+// function perguntar(pergunta) {
+//     return new Promise((resolve) => {
+//         prompt.question(pergunta, (resposta) => {
+//             const valor = Number(resposta.replace(',', '.'));
+//             resolve(valor);
+//         });
+//     });
+// }
 
-    while (isNaN(numero) || numero < 1 || numero > 3) {
-        if (numero === 0 || numero > 3 || numero < 0) {
-            console.log(posicoes[3]);
-            prompt.close();
-            return;
-        }
-        console.log("Valor inválido. Por favor, insira um número entre 1 e 3.");
-        numero = await perguntar('Digite sua posição na corrida (1, 2 ou 3): ');
-    }
+// async function medalhaDeAcordoComPosicao () {
+//     let numero = await perguntar('Digite sua posição na corrida (1, 2 ou 3): ');
+//     const posicoes = ["ouro", "prata", "bronze", "Nada"];
 
-    console.log("Parabéns! Você ganhou a medalha de");
-    console.log(posicoes[numero - 1]);
-    return medalhaDeAcordoComPosicao();
-}
-medalhaDeAcordoComPosicao();
+//     while (isNaN(numero) || numero < 1 || numero > 3) {
+//         if (numero === 0 || numero > 3 || numero < 0) {
+//             console.log(posicoes[3]);
+//             prompt.close();
+//             return;
+//         }
+//         console.log("Valor inválido. Por favor, insira um número entre 1 e 3.");
+//         numero = await perguntar('Digite sua posição na corrida (1, 2 ou 3): ');
+//     }
+
+//     console.log("Parabéns! Você ganhou a medalha de");
+//     console.log(posicoes[numero - 1]);
+//     return medalhaDeAcordoComPosicao();
+// }
+// medalhaDeAcordoComPosicao();
+
+
+// let fruit = ["Apple", "Banana", "Orange"];
+// console.log(fruit);
+// fruit.push("Kiwi");
+// console.log(fruit);
+// fruit.pop();
+// console.log(fruit);
+// fruit.unshift("Strawberry");
+// console.log(fruit);
+// fruit.shift();
+// console.log(fruit);
+
