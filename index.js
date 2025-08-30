852f0c1156920750ed387f4bd5ed7b9a9ce9463b
Marcantonio <marcantonio.lupercino@gmail.com>
2025-08-30 04:39:52 -0300

    exercicio 1

--- index.js
+++ index.js
@@ -17,58 +17,82 @@
 
 // console.log(`${jogador1.nome} tem ${jogador1.experiencia} pontos`)
 
-class contaBancaria{
-    constructor(titular,saldo){
-        this.titular = titular;
-        this.saldo = saldo;
-    }
-    depositarSaldo(depositar){
-        this.saldo += depositar;
-    }
-    sacarSaldo(sacar){
-        this.saldo -= sacar;
-    }
-    mostraSaldo(){
-        console.log (`Seu saldo é de ${this.saldo}`)
-    }
-}
+// class contaBancaria{
+//     constructor(titular,saldo){
+//         this.titular = titular;
+//         this.saldo = saldo;
+//     }
+//     depositarSaldo(depositar){
+//         this.saldo += depositar;
+//     }
+//     sacarSaldo(sacar){
+//         this.saldo -= sacar;
+//     }
+//     mostraSaldo(){
+//         console.log (`Seu saldo é de ${this.saldo}`)
+//     }
+// }
 
-const readline = require('readline');
-const prompt = readline.createInterface({
-    input: process.stdin, output: process.stdout
-});
+// const readline = require('readline');
+// const prompt = readline.createInterface({
+//     input: process.stdin, output: process.stdout
+// });
 
-function perguntar(pergunta) {
-    return new Promise((resolve) => {
-        prompt.question(pergunta, (resposta) => {
-         const valor = Number(resposta.replace(',', '.'));
-            resolve(valor);
-        });
-    });
-}
+// function perguntar(pergunta) {
+//     return new Promise((resolve) => {
+//         prompt.question(pergunta, (resposta) => {
+//          const valor = Number(resposta.replace(',', '.'));
+//             resolve(valor);
+//         });
+//     });
+// }
 
-const usuario1 = new contaBancaria("Marcos",1000);
+// const usuario1 = new contaBancaria("Marcos",1000);
 
-async function contaBancaria1() {
+// async function contaBancaria1() {
     
-    let depositar = await perguntar('Qual valor você deseja depositar? ');
-    let sacar = await perguntar('Qual valor você deseja sacar? ');
+//     let depositar = await perguntar('Qual valor você deseja depositar? ');
+//     let sacar = await perguntar('Qual valor você deseja sacar? ');
 
-    if (depositar) {
-        usuario1.depositarSaldo(depositar);
-    }
-    if (sacar) {
-        usuario1.sacarSaldo(sacar);
-    }
-    usuario1.mostraSaldo();
+//     if (depositar) {
+//         usuario1.depositarSaldo(depositar);
+//     }
+//     if (sacar) {
+//         usuario1.sacarSaldo(sacar);
+//     }
+//     usuario1.mostraSaldo();
 
-    console.log(`O Sr ${usuario1.titular} depositou ${depositar}`);
-    console.log(`O Sr ${usuario1.titular} sacou ${sacar}`);
-    console.log(`O Sr ${usuario1.titular} seu saldo é ${usuario1.mostraSaldo()}`);
-    prompt.close();
+//     console.log(`O Sr ${usuario1.titular} depositou ${depositar}`);
+//     console.log(`O Sr ${usuario1.titular} sacou ${sacar}`);
+//     console.log(`O Sr ${usuario1.titular} seu saldo é ${usuario1.mostraSaldo()}`);
+//     prompt.close();
+// }
+// contaBancaria1();
+
+class carro{
+    constructor (marca,cor,ano){
+    this.marca = marca;
+    this.cor = cor;
+    this.ano = ano;
+    this.velocidade = 0;
+    }
+    acelerar(){
+        this.velocidade += 10;
+    }
+    frear(){
+        this.velocidade -= 10;
+    }
+    mostraInfo(){
+        console.log(`O veiculo da marca: ${this.marca} de cor: ${this.cor} do ano: ${this.ano} está á ${this.velocidade} km!`);
+    }
 }
-contaBancaria1();
 
+const carro1 = new carro ("Ford","Vermelho",2015);
+const carro2 = new carro ("Chevrolet","Azul",2020);
+carro2.velocidade = 50;
 
+carro1.acelerar();
+carro1.mostraInfo();
 
-
+carro2.frear();
+carro2.mostraInfo();
