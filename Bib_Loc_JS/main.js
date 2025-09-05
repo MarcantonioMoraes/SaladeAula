0c3b342043b3fc00085a76187452dee3c2d165cf
Marcantonio <marcantonio.lupercino@gmail.com>
2025-09-05 01:30:16 -0300

    Update main.js

--- Bib_Loc_JS/main.js
+++ Bib_Loc_JS/main.js
@@ -1,7 +1,32 @@
 // node Bib_Loc_JS/main.js
+class Livro{
+    constructor(titulo, autor) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.emprestado = false;
+    }
+    emprestar() {
+        if (this.emprestado){
+            this.emprestado === true
+            console.log (`O Livro: ${this.titulo}, do autor: ${this.autor} foi emprestado ao Sr(a) $`)
+        }else{
+            console.log (`O Livro: ${this.titulo}, do autor: ${this.autor} esta disponivel!`)
+        }
+    }
+    devolver(){
+        if(!this.emprestado){
+            this.emprestado === false
+            console.log(`O Livro: ${this.titulo}, do autor: ${this.autor} foi devolvido!`)
+        }else{
+            console.log(`O Livro: ${this.titulo}, do autor: ${this.autor} não é possivel devolver!`)
+        }
+    }
+}
 
-console.log("Bib&LocJS main.js loaded");
-
+const livroA = new Livro("Os Miseráveis", "Victor Hugo.");
+const livroB = new Livro("1984", "George Orwell.");
+const livroC = new Livro("A Cabana", "William P.");
+const livroD = new Livro("Dom Casmurro", "Machado de Assis.");
 
 
 
